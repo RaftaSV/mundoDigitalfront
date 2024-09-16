a6958d1111d16efb2d14d66614aec6474e4c5577
raftasv <gp0555032019@unab.edu.sv>
2024-09-15 18:18:12 -0600

    Ajustes google analytica ajustes

--- src/index.js
+++ src/index.js
@@ -17,13 +17,14 @@ const AppRenderTheme = memo(() => {
   );
 });
 
-useEffect(() => {
-  ReactGA.initialize(`${process.env.REACT_APP_GOOGLE_ANALYTICS_ID}`);
-  ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search });
-}, []);
 
 
 const App = () => {
+
+  useEffect(() => {
+    ReactGA.initialize(`${process.env.REACT_APP_GOOGLE_ANALYTICS_ID}`);
+    ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search });
+  }, []);
   return (
     <AppThemeProvider>
       <CartProvider>
