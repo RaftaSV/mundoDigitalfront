413c9cf64603d5e646cc383ee1a3688c4803616b
raftasv <gp0555032019@unab.edu.sv>
2024-09-18 19:01:48 -0600

    ajustes rutas 1.40

--- src/routes/ProtectedRouter.js
+++ src/routes/ProtectedRouter.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {Navigate, Outlet, useLocation, useParams} from 'react-router-dom';
+import { Navigate, Outlet, useLocation, useParams } from 'react-router-dom';
 import { useAuth } from 'context/AuthContextCookie';
 import { ROUTES } from 'config';
 import usePageViews from '../hooks/insertPageView';
+
 const ProtectedRouter = () => {
   const { getCookies } = useAuth();
   const token = getCookies('auth-token');
@@ -10,28 +11,38 @@ const ProtectedRouter = () => {
   let userType = 'CLIENTE';
 
   usePageViews();
+
   if (token) {
     const payload = token.split('.')[1];
     const decoded = atob(payload);
     const userObject = JSON.parse(decoded);
     userType = userObject.User.userstype.userType.toString().toLocaleUpperCase();
   } else {
-    userType = 'CLIENTE'
+    userType = 'CLIENTE';
   }
+
   const { categoryId, productId } = useParams();
+
+  // Normaliza las rutas eliminando las barras finales y usando minúsculas para evitar problemas de comparación
+  const normalizePath = (path) => path.toLowerCase().replace(/\/+$/, '');
+
   const restrictedRoutesForClient = [
-    ROUTES.LOGIN.absolutePath,
-    ROUTES.HOME.absolutePath,
-    ROUTES.PRODUCT.absolutePath(categoryId),
-    ROUTES.PRODUCT_INFORMATION.absolutePath(productId),
-    ROUTES.CATEGORY.absolutePath,
-    ROUTES.PAYMENT_PRODUCTS.absolutePath,
+    normalizePath(ROUTES.LOGIN.absolutePath),
+    normalizePath(ROUTES.HOME.absolutePath),
+    normalizePath(ROUTES.PRODUCT.absolutePath(categoryId)),
+    normalizePath(ROUTES.PRODUCT_INFORMATION.absolutePath(productId)),
+    normalizePath(ROUTES.CATEGORY.absolutePath),
+    normalizePath(ROUTES.PAYMENT_PRODUCTS.absolutePath),
   ];
+
+  // Normaliza la ruta actual
+  const currentPath = normalizePath(location.pathname);
+
   if (userType === 'ADMIN') {
     return <Outlet />;
-  } else if (userType === 'CLIENTE' && !restrictedRoutesForClient.includes(location.pathname)){
-    console.log('retornando a home ', userType);
-    console.log('Current Path:', location.pathname);
+  } else if (userType === 'CLIENTE' && !restrictedRoutesForClient.includes(currentPath)) {
+    console.log('Retornando a home ', userType);
+    console.log('Current Path:', currentPath);
     console.log('Restricted Routes:', restrictedRoutesForClient);
 
     return <Navigate to={ROUTES.HOME.absolutePath} replace={true} />;
